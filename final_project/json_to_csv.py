5e4716819fbc7d688b42ed7f758c23769eccabab
Srihari231092 <s.srihari@outlook.com>
2019-03-22 16:52:03 -0500

    All done.

--- final_project/json_to_csv.py
+++ final_project/json_to_csv.py
@@ -5,9 +5,9 @@ import json
 
 def main():
     print(os.getcwd())
-    fpath = os.path.join(os.getcwd(), "all_uni_data.csv")
-    all_uni_data = pd.read_json(fpath, nrows=100)
-    # all_uni_data.to_csv(os.path.join(os.getcwd(), "all_uni_data.csv"))
+    fpath = os.path.join(os.getcwd(), "all_uni_data.json")
+    all_uni_data = pd.read_json(fpath, lines=True)
+    all_uni_data.to_csv(os.path.join(os.getcwd(), "all_uni_data.csv"))
 
 
 if __name__ == "__main__":
